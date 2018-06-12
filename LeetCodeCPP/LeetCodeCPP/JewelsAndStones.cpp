ac5475c8b84898d01183f1969c14fae616598e66
William Goins <de777urest@gmail.com>
2018-06-11 20:00:26 -0500

    Jewels And Stones Solution

---
+++ LeetCodeCPP/LeetCodeCPP/JewelsAndStones.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "JewelsAndStones.h"
+
+
+JewelsAndStones::JewelsAndStones()
+{
+}
+
+
+JewelsAndStones::~JewelsAndStones()
+{
+}
+
+int JewelsAndStones::numJewelsInStones(std::string J, std::string S)
+{
+	int count = 0;
+
+	for (std::string::size_type i = 0; i < J.size(); i++)
+	{
+		for (std::string::size_type j = 0; j < S.size(); j++)
+		{
+			if (J[i] == S[j])
+			{
+				count++;
+			}
+		}
+	}
+
+	return count;
+}
---
+++ LeetCodeCPP/LeetCodeCPP/JewelsAndStones.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+class JewelsAndStones
+{
+public:
+	JewelsAndStones();
+	~JewelsAndStones();
+	int numJewelsInStones(std::string, std::string);
+};
+
